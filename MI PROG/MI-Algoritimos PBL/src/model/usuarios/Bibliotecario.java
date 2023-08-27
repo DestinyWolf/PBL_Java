9f8a48d12d720cbb2025d9434726658c0f1514a8
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-08-27 12:49:24 -0300

    Create Bibliotecario.java
    Necessario a implementação dos metodos

---
+++ MI PROG/MI-Algoritimos PBL/src/model/usuarios/Bibliotecario.java
@@ -0,0 +1,14 @@
+public class Bibliotecario extends Pessoa{
+    private String Cargo;
+
+
+    public String getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(String cargo) {
+        Cargo = cargo;
+    }
+
+
+}
