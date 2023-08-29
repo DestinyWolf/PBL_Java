f239525ac93d67035fb3dc55ed03f0dddb43305f
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-08-29 15:23:17 -0300

    feature: Adição de novos DAO aos metodos

--- MI PROG/MI-Algoritimos PBL/src/dao/MasterDao.java
+++ MI PROG/MI-Algoritimos PBL/src/dao/MasterDao.java
@@ -1,16 +1,29 @@
 package dao;
 
+import dao.estoque.LivroDao;
 import dao.usuarios.ImMemoryLeitorDao;
 import dao.usuarios.LeitorDao;
+import model.usuarios.Leitor;
 
 public class MasterDao {
 
-    public static void getLeitorDAO(){
-        LeitorDao leitor = new ImMemoryLeitorDao();
+    private static LeitorDao leitorDao;
+    private static LivroDao livroDao;
+
+    public static LeitorDao getLeitorDAO(){
+        if(leitorDao == null){
+            leitorDao = new ImMemoryLeitorDao();
+        }
+        return leitorDao;
+    }
+
+    public static LivroDao getLivroDao(){
+
+        return livroDao;
     }
 
     public static void main(String[] args){
-        MasterDao.getLeitorDAO().creat
+
     }
 }
 
