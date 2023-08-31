969b25f8f0ff3b60faeaab35fcfc810dda658b3b
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-08-31 08:52:36 -0300

    Feature: criação do BibliotecarioDao e implementação

---
+++ MI PROG/MI-Algoritimos PBL/src/dao/usuarios/BibliotecarioDao.java
@@ -0,0 +1,9 @@
+package dao.usuarios;
+
+import model.usuarios.Bibliotecario;
+import dao.Dao;
+
+/**
+ * Interface das operações CRUD do operador Bibliotecario*/
+public interface BibliotecarioDao extends Dao<Bibliotecario>{
+}
---
+++ MI PROG/MI-Algoritimos PBL/src/dao/usuarios/ImMemoryBibliotecarioDao.java
@@ -0,0 +1,41 @@
+package dao.usuarios;
+
+
+import model.usuarios.Bibliotecario;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class ImMemoryBibliotecarioDao implements BibliotecarioDao{
+    private HashMap<Integer, Bibliotecario> bibliotecarios;
+
+    public ImMemoryBibliotecarioDao() {
+        this.bibliotecarios = new HashMap<>();
+    }
+    @Override
+    public Bibliotecario findById(Integer id) {
+        return this.bibliotecarios.get(id);
+    }
+
+    @Override
+    public void save(Bibliotecario obj) {
+        this.bibliotecarios.put(obj.getId(), obj);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        this.bibliotecarios.remove(id);
+    }
+
+    @Override
+    public void Update(Bibliotecario bibliotecario, Bibliotecario old) {
+        this.bibliotecarios.remove(old.getId(),old);
+        this.bibliotecarios.put(bibliotecario.getId(), bibliotecario);
+    }
+
+    @Override
+    public LinkedList<Bibliotecario> findAll() {
+        LinkedList<Bibliotecario> values = (LinkedList<Bibliotecario>) this.bibliotecarios.values();
+        return values;
+    }
+}
