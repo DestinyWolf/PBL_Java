2c8d7b732eb7a4dbaab9992f32c7f233424616f1
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-04 14:03:40 -0300

    Feature: Criação e implementação da do DAO administrador

---
+++ MI PROG/MI-Algoritimos PBL/src/dao/usuarios/AdministradorDao.java
@@ -0,0 +1,7 @@
+package dao.usuarios;
+
+import dao.Dao;
+import model.usuarios.Administrador;
+
+public interface AdministradorDao extends Dao<Administrador> {
+}
---
+++ MI PROG/MI-Algoritimos PBL/src/dao/usuarios/ImMemoryAdministradorDao.java
@@ -0,0 +1,36 @@
+package dao.usuarios;
+
+import model.usuarios.Administrador;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class ImMemoryAdministradorDao implements AdministradorDao{
+    private HashMap<Integer, Administrador> administradores;
+
+    @Override
+    public Administrador findById(Integer id) {
+        return administradores.get(id);
+    }
+
+    @Override
+    public void save(Administrador obj) {
+        administradores.put(obj.getId(), obj);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        administradores.remove(id);
+    }
+
+    @Override
+    public void Update(Administrador administrador, Administrador old) {
+        administradores.remove(old.getId());
+        administradores.put(administrador.getId(), administrador);
+    }
+
+    @Override
+    public LinkedList<Administrador> findAll() {
+        return (LinkedList<Administrador>) administradores.values();
+    }
+}
