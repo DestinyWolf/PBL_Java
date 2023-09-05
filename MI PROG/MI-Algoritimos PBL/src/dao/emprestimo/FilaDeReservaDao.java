05b66302901a048f9d828e30273de6a5cab98e6b
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-04 23:35:35 -0300

    Refactor: Alteração do nome da interface

---
+++ MI PROG/MI-Algoritimos PBL/src/dao/emprestimo/FilaDeReservaDao.java
@@ -0,0 +1,8 @@
+package dao.emprestimo;
+
+import dao.Dao;
+import model.emprestimo.FilaDeReserva;
+
+/**Interface CRUD da fila de reserva*/
+public interface FilaDeReservaDao extends Dao<FilaDeReserva> {
+}
--- MI PROG/MI-Algoritimos PBL/src/dao/emprestimo/FiladeReservaDao.java
+++
@@ -1,7 +0,0 @@
-package dao.emprestimo;
-
-import dao.Dao;
-import model.emprestimo.FilaDeReserva;
-
-public interface FiladeReservaDao extends Dao<FilaDeReserva> {
-}
