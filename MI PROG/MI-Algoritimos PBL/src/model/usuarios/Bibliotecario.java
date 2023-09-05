8b6ba3b981cef463e2e66186f2e48564932ed2ff
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-04 22:18:09 -0300

    Feature: Adição do contrutor

--- MI PROG/MI-Algoritimos PBL/src/model/usuarios/Bibliotecario.java
+++ MI PROG/MI-Algoritimos PBL/src/model/usuarios/Bibliotecario.java
@@ -5,12 +5,27 @@ package model.usuarios;
 public class Bibliotecario extends Pessoa{
     private String cargo;
 
-    //set
+
+    /**Construtor da classe
+     * @param cargo
+     * @param id
+     * @param nome
+     * @param senha */
+    public Bibliotecario(String nome, String senha, Integer id, String cargo) {
+        this.cargo = cargo;
+        super.setId(id);
+        super.setSenha(senha);
+        super.setNome(nome);
+    }
+    /**
+     * Metodo responsavel por settar o cargo do usuario
+     * @param cargo */
     public void setCargo(String cargo) {
         this.cargo = cargo;
     }
 
-    //get
+    /**Metodo reponsavel por retonar o cargo do usuario
+     * @return cargo do bibliotecario*/
     public String getCargo() {
         return cargo;
     }
