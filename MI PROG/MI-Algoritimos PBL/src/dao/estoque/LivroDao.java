2f8ee67a0ce14ff4be1fc40437f6a192e22b0cc5
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-12 09:34:18 -0300

    fix: adição das exceções aos metodos da interface LivroDao

--- MI PROG/MI-Algoritimos PBL/src/dao/estoque/LivroDao.java
+++ MI PROG/MI-Algoritimos PBL/src/dao/estoque/LivroDao.java
@@ -1,5 +1,6 @@
 package dao.estoque;
 
+import LibraryExceptions.estoqueExceptions.*;
 import dao.Dao;
 import model.estoque.Livro;
 
@@ -11,20 +12,23 @@ public interface LivroDao extends Dao<Livro> {
     /**Retornar uma lista de livros de um determinado autor
      * @param autor
      * @return Lista de livros*/
-    public LinkedList<Livro> findByAutor(String autor);
+    public LinkedList<Livro> findByAutor(String autor) throws AutorException;
 
     /**Metodo responsavel por retornar uma lista de livros de uma determinada categoria
      * @param categoria
      * @return Lista de livros*/
-    public LinkedList<Livro> findByCategoria(String categoria);
+    public LinkedList<Livro> findByCategoria(String categoria) throws CategoriaException;
 
     /**Metodo responsavel por retornar uma lista de livros com o mesmo nome
      * @param nome
      * @return Lista de livros*/
-    public LinkedList<Livro> findByNome(String nome);
+    public LinkedList<Livro> findByNome(String nome) throws FindLivroException;
+
+    public LinkedList<Livro> findByIsbn(Integer isbn) throws FindIsbnException;
 
     /**Função responsavel por deletar um unico livro dentro de uma lista de lviros com o mesmo isbn
      * @param obj */
-    public void deleteOnlyOne(Livro obj);
+    public void deleteOnlyOne(Livro obj) throws LivroDeleteException;
+
 
 }
