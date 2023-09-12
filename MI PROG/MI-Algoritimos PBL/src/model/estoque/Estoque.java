d7bb92f3b83bda1829275b2debd8ca5b06b07c1c
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-11 22:01:51 -0300

    refactor: Rfatoração do codigo/Correção de exceções

--- MI PROG/MI-Algoritimos PBL/src/model/estoque/Estoque.java
+++ MI PROG/MI-Algoritimos PBL/src/model/estoque/Estoque.java
@@ -1,5 +1,9 @@
 package model.estoque;
 
+
+import LibraryExceptions.estoqueExceptions.LivroCreateException;
+
+import LibraryExceptions.estoqueExceptions.LivroDeleteException;
 import dao.MasterDao;
 
 /**Classe model para o estoque*/
@@ -12,15 +16,24 @@ public class Estoque {
 
     /**metodo de adição de um livro novo ao estoque
      * @param livro */
-    public Livro novoLivro(Livro livro){
-        MasterDao.getLivroDao().save(livro);
-        return livro;
+    public Livro novoLivro(Livro livro) throws LivroCreateException{
+        try {
+            MasterDao.getLivroDao().save(livro);
+            return livro;
+        } catch (Exception lce) {
+            throw new LivroCreateException();
+        }
     }
 
     /**Metodo responsavel por remover um livro do estoque
      * @param livro */
-    public void RemoveLivro(Livro livro){
-        MasterDao.getLivroDao().deleteById(livro.getIsbn());
+    public void RemoveLivro(Livro livro) throws LivroDeleteException {
+        try {
+            MasterDao.getLivroDao().deleteById(livro.getIsbn());
+        } catch (Exception lde) {
+            throw new LivroDeleteException();
+        }
+
     }
 
     /**Metodo responsavel por retornar a quantidade total de livros no estoque
