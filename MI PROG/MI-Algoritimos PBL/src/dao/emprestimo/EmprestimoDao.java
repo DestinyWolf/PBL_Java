89d9cf6d784714b95d680d62b7b0960ae98d1103
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-16 19:35:30 -0300

    fix: correção das exceções da implementação do dao Emprestimo

--- MI PROG/MI-Algoritimos PBL/src/dao/emprestimo/EmprestimoDao.java
+++ MI PROG/MI-Algoritimos PBL/src/dao/emprestimo/EmprestimoDao.java
@@ -1,24 +1,25 @@
 package dao.emprestimo;
 
+import LibraryExceptions.emprestimoexception.EmprestimoException;
 import dao.Dao;
 import model.emprestimo.Emprestimo;
 import java.util.List;
 
 /**Interface CRUD da classe Emprestimo*/
-public interface EmprestimoDao extends Dao<Emprestimo, Exception> {
+public interface EmprestimoDao extends Dao<Emprestimo, EmprestimoException> {
     /**Metodo responsavel por retornar uma lista de emprestimos de um mesmo usuario
      * @param id
      * @return Lista de Emprestimos*/
-    public List<Emprestimo> findByUser(Integer id);
+    public List<Emprestimo> findByUser(Integer id) throws EmprestimoException;
 
     /**Metodo resposanvel por retornar uma lista de emprestimos de um mesmo livro
      * @param id
      * @return Lista de Emprestimos*/
-    public List<Emprestimo> findByLivro(Integer id);
+    public List<Emprestimo> findByLivro(Integer id) throws EmprestimoException;
 
     /**Metodo responsavel por retornar um emprestimo especifico
      * @param isbn
      * @param id
      * @return  Emprestimo*/
-    public Emprestimo findByUserAndLivro(Integer isbn, Integer id);
+    public Emprestimo findByUserAndLivro(Integer isbn, Integer id) throws EmprestimoException;
 }
