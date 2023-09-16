f639567b21c75b4ec23a1946446a0ba42a2e04f6
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-16 20:57:24 -0300

    fix: correção das Exceções

--- MI PROG/MI-Algoritimos PBL/src/dao/emprestimo/FilaDeReservaDao.java
+++ MI PROG/MI-Algoritimos PBL/src/dao/emprestimo/FilaDeReservaDao.java
@@ -1,8 +1,9 @@
 package dao.emprestimo;
 
+import LibraryExceptions.emprestimoexception.ReservarException;
 import dao.Dao;
 import model.emprestimo.FilaDeReserva;
 
 /**Interface CRUD da fila de reserva*/
-public interface FilaDeReservaDao extends Dao<FilaDeReserva, Exception> {
+public interface FilaDeReservaDao extends Dao<FilaDeReserva, ReservarException> {
 }
