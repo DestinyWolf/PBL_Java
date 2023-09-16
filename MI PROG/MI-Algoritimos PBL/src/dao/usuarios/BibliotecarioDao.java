3f5e99bb643c642c94d57dde3e44131c436d7868
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-16 13:09:37 -0300

    fix: Correção das exceções da interface dao BibliotecarioDao

--- MI PROG/MI-Algoritimos PBL/src/dao/usuarios/BibliotecarioDao.java
+++ MI PROG/MI-Algoritimos PBL/src/dao/usuarios/BibliotecarioDao.java
@@ -1,9 +1,10 @@
 package dao.usuarios;
 
+import LibraryExceptions.userexcepitions.BibliotecarioException;
 import model.usuarios.Bibliotecario;
 import dao.Dao;
 
 /**
  * Interface das operações CRUD do Bibliotecario*/
-public interface BibliotecarioDao extends Dao<Bibliotecario, Exception>{
+public interface BibliotecarioDao extends Dao<Bibliotecario, BibliotecarioException>{
 }
