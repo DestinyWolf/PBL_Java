4c9e823f9c48a83c69d2f5db09ebf03fbdedd05c
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-16 20:57:53 -0300

    fix: correção das Exceções

--- MI PROG/MI-Algoritimos PBL/src/dao/emprestimo/ImMemoryFilaDeReservaDao.java
+++ MI PROG/MI-Algoritimos PBL/src/dao/emprestimo/ImMemoryFilaDeReservaDao.java
@@ -1,7 +1,9 @@
 package dao.emprestimo;
 
+import LibraryExceptions.emprestimoexception.EmprestimoException;
+import LibraryExceptions.emprestimoexception.ReservarException;
 import model.emprestimo.FilaDeReserva;
-
+import static util.Constantes.*;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -13,25 +15,42 @@ public class ImMemoryFilaDeReservaDao implements FilaDeReservaDao {
         this.filasDeReserva = new HashMap<>();
     }
     @Override
-    public FilaDeReserva findById(Integer id) {
-        return filasDeReserva.get(id);
+    public FilaDeReserva findById(Integer id) throws ReservarException {
+        if(filasDeReserva.containsKey(id))
+            return filasDeReserva.get(id);
+        else {
+            throw new ReservarException(findReserva, null);
+        }
     }
 
     @Override
-    public void save(FilaDeReserva obj) {
-        this.filasDeReserva.put(obj.getIsbn(), obj);
+    public void save(FilaDeReserva obj) throws ReservarException{
+        if(this.filasDeReserva.containsKey(obj.getIsbn())) {
+            throw new ReservarException(createReserva, null);
+        }
+        else {
+            this.filasDeReserva.put(obj.getIsbn(), obj);
+        }
     }
 
     @Override
-    public void deleteById(Integer id) {
-        this.filasDeReserva.remove(id);
+    public void deleteById(Integer id) throws ReservarException{
+        if(this.filasDeReserva.containsKey(id)) {
+            this.filasDeReserva.remove(id);
+        } else {
+            throw new ReservarException(deleteReserva, null);
+        }
 
     }
 
     @Override
-    public void Update(FilaDeReserva filaDeReserva, FilaDeReserva old) {
-        this.filasDeReserva.remove(old.getIsbn());
-        this.filasDeReserva.put(filaDeReserva.getIsbn(), filaDeReserva);
+    public void Update(FilaDeReserva filaDeReserva, FilaDeReserva old) throws ReservarException{
+        if (this.filasDeReserva.containsKey(old.getIsbn())) {
+            this.filasDeReserva.remove(old.getIsbn());
+            this.filasDeReserva.put(filaDeReserva.getIsbn(), filaDeReserva);
+        } else {
+            throw new ReservarException(updateReserva, old);
+        }
     }
 
     @Override
