288458d66372ae8b6f8be839e1e054391adfd536
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-16 12:32:17 -0300

    feature: adição da classe de constantes

---
+++ MI PROG/MI-Algoritimos PBL/src/util/Constantes.java
@@ -0,0 +1,58 @@
+package util;
+
+public class Constantes {
+
+    /**Mensagens customizadas para as exceções, para facilitar no momento da implementação*/
+    public final static String deleteUser = "Impossivel deletar Usuario";
+    public final static String updateUser = "Impossivel atualizar Usuario";
+    public final static String createUser = "Impossivel criar Usuario";
+    public final static String findUser = "Impossivel encontrar Usuario";
+    public final static String loguinUser = "Senha ou Usuarios incorretos";
+    public final static String deleteLivro = "Impossivel deletar livro";
+    public final static String updateLivro = "Impossivel atualizar Livro";
+    public final static String createLivro = "Impossivel criar livro";
+    public final static String findLivro = "Impossivel encontrar livro";
+    public final static String deleteEmprestimo = "Impossivel deletar emprestimo";
+    public final static String updateEmprestimo = "Impossivel atualizar emprestimo";
+    public final static String createEmprestimo = "Impossivel criar emprestimo";
+    public final static String findEmprestimo = "Impossivel encontrar emprestimo";
+    public final static String deleteEstoque = "Impossivel deletar estoque";
+    public final static String updateEstoque = "Impossivel atualizar estoque";
+    public final static String createEstoque = "Impossivel criar estoque";
+    public final static String findEstoque = "Impossivel encontrar estoque";
+    public final static String deleteReserva = "Impossivel deletar reserva";
+    public final static String updateReserva = "Impossivel atualizar reserva";
+    public final static String createReserva = "Impossivel criar reserva";
+    public final static String findReserva = "Impossivel encontrar reserva";
+    public final static String deleteUsuarioWithEmprestimo = "Impossivel deletar, usuario possui livro sem devolução";
+    public final static String deleteLivroWithEmprestimo = "Impossivel deletar, livro esta emprestado";
+    public final static String createExistUser = "Impossivel cadastrar, Usuario já existe";
+    public final static String createReservaWithBlock = "Impossivel reservar, Usuario bloqueado";
+    public final static String createReservaWithMulta = "Impossivel reservar, Usuario multado";
+    public final static String createEmprestimoWithBlock = "Impossivel emprestar, Usuario bloqueado";
+    public final static String createEmprestimoWithMulta = "Impossivel emprestar, Usuario multado";
+    public final static String createEmprestimoWithLimiteEmprestimo = "Impossivel emprestar, limite de emprestimos atingido";
+    public final static String deleteWhenNotHaveObj = "Não há dados para serem deletados";
+    public final static String updateWhenNotHaveObj = "Não há dados para serem atualizados";
+    public final static String findWhenNotHaveObj = "Não há dados para serem encontrador";
+    public final static String inconsistenciaDeDadosForEmprestimo = "Impossivel devolver, livro não há dados de emprestimo para este usuario";
+    public final static String cpfInvalido = "Impossivel cadastrar cpf invalido";
+    public final static String renovacaoEmprestimoWithLimiteDeRenovacoes = "Impossivel renovar, limite de renovações atingido";
+    public final static String renovacaoEmprestimoWithBlock = "Impossivel renovar, Usuario Bloqueado";
+    public final static String renovacaoEmprestimoWithMulta = "Impossivel renovar, Usuario Multado";
+    public final static String renovacaoEmprestimoWithReserva = "Impossivel renovar, Livro reservado";
+    public final static String autorNotExist = "Impossivel localizar, Autor não existe";
+    public final static String categoriaNotExist = "Impossivel localizar, Categoria não existe";
+    public final static String nomeNotExist = "Impossivel localizar, nome não existe";
+    public final static String renovacaoEmprestimo = "Impossivel relizar renovação";
+    public final static String reservaLivro = "Impossivel realizar reserva";
+
+
+
+
+
+
+
+
+
+}
