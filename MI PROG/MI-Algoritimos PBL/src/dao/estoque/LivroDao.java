700603ef8dcbf7be6053ff5b9ebfa5db7fb0d952
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-16 12:33:17 -0300

    feature: adição das exceções ao LivroDao

--- MI PROG/MI-Algoritimos PBL/src/dao/estoque/LivroDao.java
+++ MI PROG/MI-Algoritimos PBL/src/dao/estoque/LivroDao.java
@@ -1,34 +1,35 @@
 package dao.estoque;
 
 import LibraryExceptions.estoqueExceptions.*;
+import LibraryExceptions.userexcepitions.LeitorException;
 import dao.Dao;
 import model.estoque.Livro;
 
 import java.util.LinkedList;
 
 /**Interface das operações CRUD do Livro*/
-public interface LivroDao extends Dao<Livro, Exception> {
+public interface LivroDao extends Dao<Livro, LivroException> {
 
     /**Retornar uma lista de livros de um determinado autor
      * @param autor
      * @return Lista de livros*/
-    public LinkedList<Livro> findByAutor(String autor) throws AutorException;
+    public LinkedList<Livro> findByAutor(String autor) throws LivroException;
 
     /**Metodo responsavel por retornar uma lista de livros de uma determinada categoria
      * @param categoria
      * @return Lista de livros*/
-    public LinkedList<Livro> findByCategoria(String categoria) throws CategoriaException;
+    public LinkedList<Livro> findByCategoria(String categoria) throws LivroException;
 
     /**Metodo responsavel por retornar uma lista de livros com o mesmo nome
      * @param nome
      * @return Lista de livros*/
-    public LinkedList<Livro> findByNome(String nome) throws FindLivroException;
+    public LinkedList<Livro> findByNome(String nome) throws LivroException;
 
-    public LinkedList<Livro> findByIsbn(Integer isbn) throws FindIsbnException;
+    public LinkedList<Livro> findByIsbn(Integer isbn) throws LivroException;
 
     /**Função responsavel por deletar um unico livro dentro de uma lista de lviros com o mesmo isbn
      * @param obj */
-    public void deleteOnlyOne(Livro obj) throws LivroDeleteException;
+    public void deleteOnlyOne(Livro obj) throws LivroException;
 
 
 }
