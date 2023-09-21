5a9aed7f58dc04cd3fc5cde67f53aaad3decef09
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-21 02:14:27 -0300

    feature: Adição de um novo texto para as exceções

--- MI PROG/MI-Algoritimos PBL/src/util/Constantes.java
+++ MI PROG/MI-Algoritimos PBL/src/util/Constantes.java
@@ -47,6 +47,7 @@ public class Constantes {
     public final static String renovacaoEmprestimo = "Impossivel relizar renovação";
     public final static String reservaLivro = "Impossivel realizar reserva";
     public final static String deleteEmprestimoWhenIsNotDevolvido = "Impossivel deletar, emprestimo não devolvido";
+    public final static String devolucaoError = "Impossivel realizar devolução";
 
 
 
