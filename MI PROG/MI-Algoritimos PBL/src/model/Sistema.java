01f1b1b6455bec88c5ef2e70f7e24b71d0b8a5fb
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-24 11:43:06 -0300

    feature: adição de metodo de atualização de emprestimos

--- MI PROG/MI-Algoritimos PBL/src/model/Sistema.java
+++ MI PROG/MI-Algoritimos PBL/src/model/Sistema.java
@@ -1,10 +1,26 @@
 package model;
 
+import LibraryExceptions.emprestimoexception.EmprestimoException;
+import dao.MasterDao;
+import model.emprestimo.Emprestimo;
+import util.Data;
+
 public class Sistema {
 
 
     public static boolean AtualizaEmprestimos() {
+        Data dataAtual = new Data();
+        try {
+            for (Emprestimo emprestimo : MasterDao.getEmprestimoDao().findEmprestimosAtivos()
+                ){
 
+                if (emprestimo.getPrazoFinal().compareData(dataAtual) == 2) {
+                    emprestimo.getLeitor().setDiasRestantesMulta(emprestimo.getLeitor().getDiasRestantesMulta()+ 2);
+                }
+            }
+        } catch (EmprestimoException e) {
+
+        }
         return false;
     }
 }
