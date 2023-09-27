c37bbdf0b59ab46416f5ae1ff8b94e42b1db157c
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-27 13:06:19 -0300

    feture: adição de metodos e atributos para sessao atual

--- MI PROG/MI-Algoritimos PBL/src/model/Sistema.java
+++ MI PROG/MI-Algoritimos PBL/src/model/Sistema.java
@@ -3,12 +3,21 @@ package model;
 import LibraryExceptions.emprestimoexception.EmprestimoException;
 import dao.MasterDao;
 import model.emprestimo.Emprestimo;
+import model.usuarios.Administrador;
+import model.usuarios.Bibliotecario;
 import model.usuarios.Leitor;
 import util.Data;
 
 public class Sistema {
 
+    public static Leitor sessaoAtualLeitor;
+    public static Bibliotecario sessaoAtualBibliotecario;
+    public static Administrador sessaoAtualAdministrador;
 
+
+    /**
+     * metodo responsavel por, toda vez que o sistema iniciar, calcular as multas
+     * @return true, se tudo ocoreu certo | false caso ocorra algum erro*/
     public static boolean atualizaMultas() {
         Data dataAtual = new Data();
         try {
@@ -51,4 +60,27 @@ public class Sistema {
 
     }
 
+    public static Leitor getSessaoAtualLeitor() {
+        return sessaoAtualLeitor;
+    }
+
+    public static Administrador getSessaoAtualAdministrador() {
+        return sessaoAtualAdministrador;
+    }
+
+    public static Bibliotecario getSessaoAtualBibliotecario() {
+        return sessaoAtualBibliotecario;
+    }
+
+    public static void setSessaoAtualAdministrador(Administrador sessaoAtualAdministrador) {
+        Sistema.sessaoAtualAdministrador = sessaoAtualAdministrador;
+    }
+
+    public static void setSessaoAtualBibliotecario(Bibliotecario sessaoAtualBibliotecario) {
+        Sistema.sessaoAtualBibliotecario = sessaoAtualBibliotecario;
+    }
+
+    public static void setSessaoAtualLeitor(Leitor sessaoAtualLeitor) {
+        Sistema.sessaoAtualLeitor = sessaoAtualLeitor;
+    }
 }
