3146a1eb2a2b9d8f0f3f47c7c70e3e96a4156794
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-09-28 00:56:02 -0300

    test: correção de teste

--- MI PROG/MI-Algoritimos PBL/src/Testes/dao/ImMemoryLeitorDaoTest.java
+++ MI PROG/MI-Algoritimos PBL/src/Testes/dao/ImMemoryLeitorDaoTest.java
@@ -1,7 +1,11 @@
 package Testes.dao;
 
 import LibraryExceptions.userexcepitions.LeitorException;
+import dao.MasterDao;
+import dao.emprestimo.ImMemoryEmprestimoDao;
 import dao.usuarios.ImMemoryLeitorDao;
+import model.emprestimo.Emprestimo;
+import model.estoque.Livro;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,9 +14,19 @@ import model.usuarios.Leitor;
 
 class ImMemoryLeitorDaoTest {
     private Leitor leitor;
+    private ImMemoryLeitorDao dao;
+
+
+
     @BeforeEach
     void criar () throws Exception{
-        leitor = new Leitor("Joao","123","57130521090","UEFS","0000");
+        leitor = new Leitor("Maike","123","62909475085","UEFS",
+                "75 9 88888888");
+        dao = new ImMemoryLeitorDao();
+        MasterDao.getLivroDao().save(new Livro(1,"bla", "horror", "pat", "saraiva",2000,"as aventuras de creitin"));
+
+
+
     }
 
 
@@ -20,50 +34,52 @@ class ImMemoryLeitorDaoTest {
 
     @Test
     void save() throws LeitorException, Exception {
-        ImMemoryLeitorDao dao = new ImMemoryLeitorDao();
         dao.save(leitor);
-        assertEquals(leitor,dao.findById(Integer.parseInt(leitor.getId())));
+        assertEquals(leitor,dao.findByCpf(leitor.getId()));
     }
 
     @Test
-    void deleteById() throws LeitorException, Exception{
+    void delete() throws LeitorException, Exception{
 
-        ImMemoryLeitorDao dao = new ImMemoryLeitorDao();
         dao.save(leitor);
-        dao.delete(Integer.parseInt(leitor.getId()));
+        dao.delete(leitor);
 
 
     }
 
     @Test
     void update() throws LeitorException, Exception{
-        ImMemoryLeitorDao dao = new ImMemoryLeitorDao();
+
         dao.save(leitor);
-        Leitor leitor2 = new Leitor("Pedro","222","57130521091","SAP","000");
+        Leitor leitor2 = new Leitor("Pedro","222","21780640005","SAP","000");
         dao.Update(leitor2,leitor);
-        dao.findById(Integer.parseInt(leitor2.getId()));
+        dao.findByCpf(leitor2.getId());
     }
 
     @Test
     void findById() throws LeitorException, Exception{
-        ImMemoryLeitorDao dao = new ImMemoryLeitorDao();
+
         dao.save(leitor);
-        assertEquals(leitor,dao.findById(Integer.parseInt(leitor.getId())));
+        assertEquals(leitor,dao.findByCpf(leitor.getId()));
     }
 
     @Test
     void findAll() throws LeitorException, Exception{
-        ImMemoryLeitorDao dao = new ImMemoryLeitorDao();
+
         dao.save(leitor);
         assertEquals(1,dao.findAll().size());
     }
 
     @Test
     void testFindLogin() throws LeitorException, Exception {
-        ImMemoryLeitorDao dao = new ImMemoryLeitorDao();
         dao.save(leitor);
-        Leitor f = dao.findLogin(Integer.parseInt(leitor.getId()), leitor.getSenha());
+        Leitor f = dao.findLogin(leitor.getId(), leitor.getSenha());
         assertNotNull(f);
+    }
+
+    @Test
+    void findByCpfTest() throws LeitorException{
+
     }
 
 }
