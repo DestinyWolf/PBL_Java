5f20edb489f1ddfb6da3d1466c9595737a41b1c0
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-09-29 14:24:06 -0300

    Test:Atualização em ImMemoryBibliotecarioDaoTest

--- MI PROG/MI-Algoritimos PBL/src/Testes/dao/ImMemoryBibliotecarioDaoTest.java
+++ MI PROG/MI-Algoritimos PBL/src/Testes/dao/ImMemoryBibliotecarioDaoTest.java
@@ -1,20 +1,23 @@
 package Testes.dao;
 
+import LibraryExceptions.userexcepitions.BibliotecarioException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import dao.usuarios.ImMemoryBibliotecarioDao;
 import model.usuarios.Bibliotecario;
+
+
 class ImMemoryBibliotecarioDaoTest {
 
     private ImMemoryBibliotecarioDao dao;
     private Bibliotecario bibi;
 
     @BeforeEach
-    void criar(){
+    void criar() throws BibliotecarioException {
         dao = new ImMemoryBibliotecarioDao();
-
+        bibi = new Bibliotecario("ArmandoTest","123","64299259513","Brabo");
     }
 
     @Test
