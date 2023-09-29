31b7d137b969e511f043a0e3da39fcb1344a117d
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-09-29 14:20:59 -0300

    Test: Criando ImMemoryBibliotecarioDaoTest

---
+++ MI PROG/MI-Algoritimos PBL/src/Testes/dao/ImMemoryBibliotecarioDaoTest.java
@@ -0,0 +1,43 @@
+package Testes.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import dao.usuarios.ImMemoryBibliotecarioDao;
+import model.usuarios.Bibliotecario;
+class ImMemoryBibliotecarioDaoTest {
+
+    private ImMemoryBibliotecarioDao dao;
+    private Bibliotecario bibi;
+
+    @BeforeEach
+    void criar(){
+
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findLogin() {
+    }
+}
