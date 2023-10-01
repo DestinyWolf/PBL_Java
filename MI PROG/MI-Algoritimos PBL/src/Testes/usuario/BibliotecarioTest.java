061ecdb0dbe75fb35ecb2ddbfc655ee1d2bd980a
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-10-01 10:31:20 -0300

    Test: Atualização bibliotecariotest

--- MI PROG/MI-Algoritimos PBL/src/Testes/usuario/BibliotecarioTest.java
+++ MI PROG/MI-Algoritimos PBL/src/Testes/usuario/BibliotecarioTest.java
@@ -2,12 +2,18 @@ package Testes.usuario;
 
 import LibraryExceptions.userexcepitions.BibliotecarioException;
 import model.usuarios.Bibliotecario;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BibliotecarioTest {
-    private Bibliotecario b = new Bibliotecario("Armando","123","57130521090","Lider");
+    private Bibliotecario b;
+
+    @BeforeEach
+            void criar() throws BibliotecarioException{
+        b = new Bibliotecario("Armando","123","57130521090","Lider");
+    }
 
     BibliotecarioTest() throws BibliotecarioException {
     }
