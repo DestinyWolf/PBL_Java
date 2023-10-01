9265c4a7c1de9c4ec5ed34a1faef775cc7e6065c
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2023-10-01 10:32:36 -0300

    Test: Atualização AdministradorTest

--- MI PROG/MI-Algoritimos PBL/src/Testes/usuario/AdministradorTest.java
+++ MI PROG/MI-Algoritimos PBL/src/Testes/usuario/AdministradorTest.java
@@ -1,16 +1,19 @@
 package Testes.usuario;
 
 import LibraryExceptions.userexcepitions.AdministradorException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import model.usuarios.Administrador;
 
 class AdministradorTest {
-    Administrador a = new Administrador("123","Ken","Maximo","57130521090");
-
-    AdministradorTest() throws AdministradorException {
+    private Administrador a;
+    @BeforeEach
+    void criar() throws AdministradorException{
+        a = new Administrador("123","Ken","Maximo","57130521090");
     }
+
 
     @Test
     void getCargo() {
