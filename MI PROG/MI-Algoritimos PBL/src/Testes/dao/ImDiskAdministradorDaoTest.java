591efb34cdafb7a87d6689bb531a6ad0c9919268
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-11-28 20:58:50 -0300

    fix: correção de função de busca

--- MI PROG/MI-Algoritimos PBL/src/Testes/dao/ImDiskAdministradorDaoTest.java
+++ MI PROG/MI-Algoritimos PBL/src/Testes/dao/ImDiskAdministradorDaoTest.java
@@ -19,17 +19,19 @@ public class ImDiskAdministradorDaoTest {
 
     @BeforeEach
     void setUp() throws Exception {
-        FileManeger.generateCache();
+        while(MasterDao.getAdministradorDao().findAll().size() > 0) {
+            MasterDao.getAdministradorDao().delete(MasterDao.getAdministradorDao().findAll().get(0));
+        }
         adm = new Administrador("123","Armando","Ditador","57130521090");
         dao = new ImDiskAdministradorDao();
         Novoadm = new Administrador("222","MaikeTest","Adm","11130521333");
 
-        MasterDao.getAdministradorDao().save(adm);
     }
 
     @Test
     void findById() throws AdministradorException {
-        assertEquals(adm.getId(),dao.findById(adm.getId()).getId());
+        dao.save(adm);
+        assertEquals(adm,dao.findById(adm.getId()));
     }
 
     @Test
@@ -40,23 +42,30 @@ public class ImDiskAdministradorDaoTest {
 
     @Test
     void delete() throws AdministradorException{
+        dao.save(adm);
         dao.delete(adm);
         assertEquals(0, dao.findAll().size());
     }
 
     @Test
     void update() throws AdministradorException {
+        dao.save(adm);
         dao.Update(Novoadm,adm);
         dao.findById(Novoadm.getId());
+        assertNotEquals(adm,dao.findById(Novoadm.getId()));
     }
 
     @Test
     void findAll() throws AdministradorException{
+        dao.save(adm);
         assertEquals(1, dao.findAll().size());
     }
 
     @Test
     void findLogin() throws AdministradorException{
-        dao.findLogin(adm.getId(),adm.getSenha());
+        dao.save(adm);
+        dao.save(Novoadm);
+        Administrador sessao = dao.findLogin(adm.getId(),adm.getSenha());
+        assertEquals(adm, sessao);
     }
 }
