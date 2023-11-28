baf1117348aea6f33b026a2e92b70a8a587febce
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-11-28 20:41:38 -0300

    test:correção e refatoramente do teste, ainda há um bu ao rodar todos os testes, mas os testes individuais funcionam

--- MI PROG/MI-Algoritimos PBL/src/Testes/dao/ImDiskLeitorDaoTest.java
+++ MI PROG/MI-Algoritimos PBL/src/Testes/dao/ImDiskLeitorDaoTest.java
@@ -7,11 +7,11 @@ import dao.usuarios.ImDiskLeitorDao;
 import dao.usuarios.ImMemoryLeitorDao;
 import model.estoque.Livro;
 import model.usuarios.Leitor;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ImDiskLeitorDaoTest {
     private Leitor leitor;
@@ -20,24 +20,45 @@ class ImDiskLeitorDaoTest {
 
 
     @BeforeEach
-    void criar () throws Exception{
-        FileManeger.generateCache();
+    void setUp () throws Exception{
+        while(MasterDao.getLeitorDAO().findAll().size() > 0) {
+            MasterDao.getLeitorDAO().delete(MasterDao.getLeitorDAO().findAll().get(0));
+        }
         leitor = new Leitor("Maike","123","62909475085","UEFS",
                 "75 9 88888888");
         dao = new ImDiskLeitorDao();
         MasterDao.getLivroDao().save(new Livro("1","bla", "horror", "pat", "saraiva",2000,"as aventuras de creitin"));
     }
 
+    @AfterEach
+    void setDown() throws Exception{
+        while(MasterDao.getLeitorDAO().findAll().size() > 0) {
+            MasterDao.getLeitorDAO().delete(MasterDao.getLeitorDAO().findAll().get(0));
+        }
+
+    }
+
     @Test
     void save() throws LeitorException, Exception {
-        dao.save(leitor);
-        assertEquals(leitor,dao.findById(leitor.getId()));
+
+        Leitor leitor1 = new Leitor("Carlinhos", "123", "18011808060","sua casa", "75 9 92323232");
+
+        dao.save(leitor1);
+
+        assertEquals(leitor1,dao.findById(leitor1.getId()));
+
+
     }
 
     @Test
     void delete() throws LeitorException, Exception{
+        Leitor leitor1 = new Leitor("Carlinhos", "123", "18011808060","sua casa", "75 9 92323232");
+
         dao.save(leitor);
+        dao.save(leitor1);
         dao.delete(leitor);
+
+        assertEquals(1,dao.findAll().size());
 
 
     }
@@ -47,7 +68,7 @@ class ImDiskLeitorDaoTest {
         dao.save(leitor);
         Leitor leitor2 = new Leitor("Pedro","222","21780640005","SAP","000");
         dao.Update(leitor2,leitor);
-        dao.findById(leitor2.getId());
+        assertNotEquals(leitor, dao.findById(leitor2.getId()));
     }
 
     @Test
@@ -68,7 +89,7 @@ class ImDiskLeitorDaoTest {
     void testFindLogin() throws LeitorException, Exception {
         dao.save(leitor);
         Leitor f = dao.findLogin(leitor.getId(), leitor.getSenha());
-        assertNotNull(f);
+        assertEquals(leitor, f);
     }
 
 }
