086db77f60984bbf2447c235083fb0527b82dc95
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-12-04 19:54:56 -0300

    docs: adição e atualização dos comentarios

--- MI PROG/MI-Algoritimos PBL/src/model/Sistema.java
+++ MI PROG/MI-Algoritimos PBL/src/model/Sistema.java
@@ -8,6 +8,8 @@ import model.usuarios.Bibliotecario;
 import model.usuarios.Leitor;
 import util.Data;
 
+import java.util.LinkedList;
+
 public class Sistema {
 
     public static Leitor sessaoAtualLeitor;
@@ -16,8 +18,8 @@ public class Sistema {
 
 
     /**
-     * metodo responsavel por, toda vez que o sistema iniciar, calcular as multas
-     * @return true, se tudo ocoreu certo | false caso ocorra algum erro*/
+     * <p>Metodo responsavel por, toda vez que o sistema iniciar, calcular as multas</p>
+     * @return <b>boolean</b> - <i><b>true</b>, se tudo ocoreu certo, <b>false</b>, caso ocorra algum erro</i>*/
     public static boolean atualizaMultas() {
         Data dataAtual = new Data();
         try {
@@ -60,27 +62,52 @@ public class Sistema {
 
     }
 
+    /**
+     * <p>metodo responsavel por retornar o leitor que esta logado no atual momento</p>
+     * @return <b>Leitor</b> - <i>o leitor atual logado</i>
+     */
     public static Leitor getSessaoAtualLeitor() {
         return sessaoAtualLeitor;
     }
 
+    /**
+     * <p>metodo responsavel por retornar o administrador que esta logado no atual momento</p>
+     * @return <b>Administrador</b> - <i>o administrador atual logado</i>
+     */
     public static Administrador getSessaoAtualAdministrador() {
         return sessaoAtualAdministrador;
     }
 
+    /**
+     * <p>metodo responsavel por retornar o bibliotecario que esta logado no atual momento</p>
+     * @return <b>Bibliotecario</b> - <i>o bibliotecario atual logado</i>
+     */
     public static Bibliotecario getSessaoAtualBibliotecario() {
         return sessaoAtualBibliotecario;
     }
 
+    /**
+     * <p>Metodo responsavel por settar o administrador passado por parametro como administrador atual logado</p>
+     * @param sessaoAtualAdministrador <b>Administrador</b>
+     */
     public static void setSessaoAtualAdministrador(Administrador sessaoAtualAdministrador) {
         Sistema.sessaoAtualAdministrador = sessaoAtualAdministrador;
     }
 
+    /**
+     * <p>Metodo responsavel por settar o bibliotecario passado por parametro como bibliotecario atual logado</p>
+     * @param sessaoAtualBibliotecario <b>Bibliotecario</b>
+     */
     public static void setSessaoAtualBibliotecario(Bibliotecario sessaoAtualBibliotecario) {
         Sistema.sessaoAtualBibliotecario = sessaoAtualBibliotecario;
     }
 
+    /**
+     * <p>Metodo responsavel por settar o leitor passado por parametro como leitor atual logado</p>
+     * @param sessaoAtualLeitor
+     */
     public static void setSessaoAtualLeitor(Leitor sessaoAtualLeitor) {
         Sistema.sessaoAtualLeitor = sessaoAtualLeitor;
     }
+
 }
