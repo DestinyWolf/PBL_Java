914d717f4bdf99eb3b96141bf725902975227d9e
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-12-04 22:10:44 -0300

    docs: adição e atualização dos comentarios

--- MI PROG/MI-Algoritimos PBL/src/dao/estoque/LivroDao.java
+++ MI PROG/MI-Algoritimos PBL/src/dao/estoque/LivroDao.java
@@ -7,43 +7,43 @@ import model.estoque.Livro;
 import java.util.LinkedList;
 import java.util.List;
 
-/**Interface das operações CRUD do Livro*/
+/**<p>Interface das operações CRUD do Livro</p>*/
 public interface LivroDao extends Dao<Livro, LivroException> {
 
     /**
-     * Retornar uma lista de livros de um determinado autor
-     * @param autor
-     * @return LinkedList de livros
-     * @throws LivroException
+     * <p>Metodo responsavel por retornar uma <b>lista de livros de um autor</b></p>
+     * @param autor <b>String</b>
+     * @return <b>List&lt;Livros&gt;</b> - <i>Lista de livros</i>
+     * @throws LivroException <i>Caso não seja encontrado o autor</i>
      */
     public List<Livro> findByAutor(String autor) throws LivroException;
 
     /**
-     * Metodo responsavel por retornar uma lista de livros de uma determinada categoria
-     * @param categoria
-     * @return LinkedList de livros
-     * @throws LivroException
+     * <p>Metodo responsavel por retornar uma <b>lista de livros de uma categoria</b></p>
+     * @param categoria <b>String</b>
+     * @return <b>List&lt;Livros&gt;</b> - <i>Lista de livros</i>
+     * @throws LivroException <i>Caso não seja achado determinada categoria</i>
      */
     public List<Livro> findByCategoria(String categoria) throws LivroException;
 
     /**
-     * Metodo responsavel por retornar uma lista de livros com o mesmo nome
-     * @param nome
-     * @return LinkedList de livros
-     * @throws LivroException
+     * <p>Metodo responsavel por retornar uma <b>lista de livros com o mesmo nome</b></p>
+     * @param nome <b>String</b>
+     * @return <b>List&lt;Livros&gt;</b> - <i>Lista de livros</i>
+     * @throws LivroException <i>Caso não seja encontrado livros com determinado nome</i>
      */
     public List<Livro> findByNome(String nome) throws LivroException;
 
     /**
-     * Metodo responsavel por remover um livro da quantidae total de livros
-     * @param obj
-     * @throws LivroException
+     * <p>Metodo responsavel por <b>remover um livro da quantidae total de livros</b></p>
+     * @param obj <b>Livro</b>
+     * @throws LivroException <i> caso não tenha nenhum livro ou caso não seja encontrado</i>
      */
     public void deleteOnlyOne(Livro obj) throws LivroException;
 
     /**
-     * retonar uma lista de livros com os livros mais pesquisados
-     * @return List de livros
+     * <p>Metodo responsavel por retonar uma <b>lista de livros com os livros mais pesquisados</b></p>
+     * @return <b>List&lt;Livros&gt;</b> - <i>Lista de livros</i>
      */
     public List<Livro> findLivrosMaisPesquisados();
 
