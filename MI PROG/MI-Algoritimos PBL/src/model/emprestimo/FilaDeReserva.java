868fd8a57d1fade0a8476cb1bae9dee9219bc8e8
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-12-04 21:17:36 -0300

    docs: adição e atualização dos comentarios

--- MI PROG/MI-Algoritimos PBL/src/model/emprestimo/FilaDeReserva.java
+++ MI PROG/MI-Algoritimos PBL/src/model/emprestimo/FilaDeReserva.java
@@ -10,47 +10,48 @@ import java.util.Queue;
 
 import static util.Constantes.*;
 
-/**Classe model para a fila de reserva*/
+/**<p>Classe model para a fila de reserva</p>*/
 public class FilaDeReserva implements Serializable {
     private Queue<Leitor> reservas;
     private String isbn;
 
-    /**Construtor da classe Fila de reserva
-     * @param isbn*/
+    /**<p>Construtor da classe Fila de reserva, é necessario ter um livro pra poder criar uma nova fila de reserva</p>
+     * @param isbn <b>String</b>*/
     public FilaDeReserva(String isbn) {
         this.reservas = new LinkedList<>();
         this.isbn = isbn;
     }
 
-    /**Metodo responsavel por retornar o isbn do livro daquela fila de reserva
-     * @return  isbn do livro*/
+    /**<p>Metodo responsavel por retornar o <b>codigo isbn</b> do livro da fila de reserva</p>
+     * @return <b>String</b> - <i>codigo isbn do livro</i> */
     public String getIsbn() {
         return isbn;
     }
 
     /**
-     * Metodo responsavel por retornar as reservas dos leitores
-     * @return Fila de leitores que reservaram esse livro*/
+     * <p>Metodo responsavel por retornar a <b>Lista de reserva</b> do livro</p>
+     * @return <b>Queue&lt;Leitor&gt;</b> - <i>Fila de leitores que reservaram esse livro</i>*/
     public Queue<Leitor> getReservas() {
         return reservas;
     }
 
-    /**Metodo responsavel por settar o isbn do livro
-     * @param isbn*/
+    /**<p>Metodo responsavel por settar o <b>codigo isbn</b> do livro</p>
+     * @param isbn <b>String</b>*/
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
-    /**Metodo responsavel por settar as reservas do livro
-     * @param reservas */
+    /**<p>Metodo responsavel por settar a <b>fila de reserva</b> do livro</p>
+     * @param reservas <b>Queue&lt;Leitor&gt;</b>*/
     public void setReservas(Queue<Leitor> reservas) {
         this.reservas = reservas;
     }
 
     /**
-     * Metodo responsavel por adicionar um leitor a fila de reserva
-     * @param leitor
-     * @throws ReservarException
+     * <p>Metodo responsavel por <b>adicionar um leitor</b> a fila de reserva</p>
+     * @param leitor <b>Leitor</b>
+     * @throws ReservarException <i>caso tente adicionar um leitor que esteja bloqueado, com multa, ou ocorra
+     * algum problema no momento da adição</i>
      */
     public void addOnReservas(Leitor leitor) throws ReservarException{
         if (!leitor.isBloqueio() && leitor.getDiasRestantesMulta() == 0) {
@@ -65,9 +66,9 @@ public class FilaDeReserva implements Serializable {
     }
 
     /**
-     * Metodo responsavel por remover o leitor que esta no topo da fila de reserva
-     * @return o leitor que é o proximo a poder pegar o livro
-     * @throws ReservarException
+     * <p>Metodo responsavel por <b>remover o leitor</b> que será o <b>proximo</b>, da fila de reserva, a pegar o livro</p>
+     * @return <b>Leitor</b> - <i>Leitor que ta no topo da fila</i>
+     * @throws ReservarException <i>Caso ocorra algum problema no momento de remover ou retornar o leitor</i>
      */
     public Leitor removeOnReservas() throws ReservarException{
 
