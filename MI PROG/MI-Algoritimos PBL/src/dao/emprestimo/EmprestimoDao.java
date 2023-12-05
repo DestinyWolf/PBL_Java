3693d8995f8bf59f1a1a1e758e7a4337c35f31d5
Maike <64764136+DestinyWolf@users.noreply.github.com>
2023-12-04 22:10:47 -0300

    docs: adição e atualização dos comentarios

--- MI PROG/MI-Algoritimos PBL/src/dao/emprestimo/EmprestimoDao.java
+++ MI PROG/MI-Algoritimos PBL/src/dao/emprestimo/EmprestimoDao.java
@@ -8,49 +8,50 @@ import java.util.List;
 /**Interface CRUD da classe Emprestimo*/
 public interface EmprestimoDao extends Dao<Emprestimo, EmprestimoException> {
     /**
-     * Metodo responsavel por retornar uma lista de emprestimos de um mesmo usuario
-     * @param id
-     * @return List de emprestimos
-     * @throws EmprestimoException
+     * <p>Metodo responsavel por retornar uma <b>lista de emprestimos de um mesmo usuario</b> a partir de seu <b>id</b></p>
+     * @param id <b>String</b>
+     * @return <b>List&lt;Emprestimo&gt;</b> - <i>Lista de Emprestimos</i>
+     * @throws EmprestimoException <i>Caso não seja encontrado nenhum emprestimo ou nenhum usuario</i>
      */
     public List<Emprestimo> findByUser(String id) throws EmprestimoException;
 
     /**
-     * Metodo resposanvel por retornar uma lista de emprestimos de um mesmo livro
-     * @param id
-     * @return List de emprestimos
-     * @throws EmprestimoException
+     * <p>Metodo resposanvel por retornar uma <b>lista de emprestimos de um mesmo livro</b> a partir de seu <b>codigo ISBN</b></p>
+     * @param id <b>String</b>
+     * @return <b>List&lt;Emprestimo&gt;</b> - <i>Lista de Emprestimos</i>
+     * @throws EmprestimoException <i>Caso não seja encontrado nenhum emprestimo ou nenhum livro</i>
      */
     public List<Emprestimo> findByLivro(String id) throws EmprestimoException;
 
     /**
-     * Metodo responsavel por retornar um emprestimo especifico
-     * @param isbn
-     * @param id
-     * @return Emprestimo
-     * @throws EmprestimoException
+     * <p>Metodo responsavel por retornar um <b>emprestimo especifico</b></p>
+     * @param isbn <b>String</b>
+     * @param id <b>String</b>
+     * @return <b>Emprestimo</b> - <i>Emprestimo referente a esse livro e esse id de usuario</i>
+     * @throws EmprestimoException <i> caso não seja impossivel encontrar este emprestimo
+     * ou os dados fornecidos sejam invalidos</i>
      */
     public Emprestimo findByUserAndLivro(String isbn, String id) throws EmprestimoException;
 
     /**
-     * Metodo responsavel por retonar os emprestimos ativos de um determinado usuario
-     * @param id
-     * @return List de Emprestimos
-     * @throws EmprestimoException
+     * <p>Metodo responsavel por retonar os <b>emprestimos ativos de um usuario</b></p>
+     * @param id <b>String</b>
+     * @return <b>List&lt;Emprestimo&gt;</b> - <i>Lista de Emprestimos</i>
+     * @throws EmprestimoException <i>Caso não seja encontrado nenhum emprestimo ativo, ou dados invalidos</i>
      */
     public List<Emprestimo> findEmprestimosAtivosPorUsuario(String id) throws EmprestimoException;
 
     /**
-     * Metodo responsavel por retonar todos os emprestimos ativos
-     * @return List de Emprestimos
-     * @throws EmprestimoException
+     * <p>Metodo responsavel por retonar <b>todos os emprestimos ativos</b></p>
+     * @return <b>List&lt;Emprestimo&gt;</b> - <i>Lista de Emprestimos</i>
+     * @throws EmprestimoException <i>Caso não haja nenhum emprestimo ativo, ou cadastrado</i>
      */
     public List<Emprestimo> findEmprestimosAtivos() throws EmprestimoException;
 
     /**
-     * Metodo responsavel por retornar a lista com todos os emprestimos encerrados
-     * @return List de Emprestimos
-     * @throws EmprestimoException
+     * <p>Metodo responsavel por retornar a lista com <b>todos os emprestimos encerrados</b></p>
+     * @return <b>List&lt;Emprestimo&gt;</b> - <i>Lista de Emprestimos</i>
+     * @throws EmprestimoException <i>Caso não hja nenhum emprestimo encerrado, ou cadastrado</i>
      */
     public List<Emprestimo> findEmprestimosEncerrados() throws EmprestimoException;
 }
